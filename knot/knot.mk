d5c879907f563a3ee687d40f1f34a52595b5dafd
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-10 19:03:10 +0200

    fix dependency

--- knot/knot.mk
+++ knot/knot.mk
@@ -7,6 +7,6 @@
 KNOT_VERSION = 3.1.7
 KNOT_SITE = https://secure.nic.cz/files/knot-dns
 KNOT_SOURCE = knot-$(KNOT_VERSION).tar.xz
-KNOT_DEPENDENCIES = gnutls libedit liburcu lmdb
+KNOT_DEPENDENCIES = gnutls libedit liburcu liblmdb
 
 $(eval $(autotools-package))
