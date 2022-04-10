b2413cc12bfb945bf9970f0f37f59fa11fe3ffc9
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-10 19:37:51 +0200

    change git:// to https://

--- liblmdb/liblmdb.mk
+++ liblmdb/liblmdb.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 LIBLMDB_VERSION = mdb.master
-LIBLMDB_SITE = git://git.openldap.org/openldap/openldap
+LIBLMDB_SITE = https://git.openldap.org/openldap/openldap.git
+LIBLMDB_SITE_METHOD = git
 
 define LIBLMDB_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
