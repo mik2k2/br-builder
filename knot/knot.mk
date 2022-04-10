4db83bf4b5438e0f60a8540e3d88a0c02e9ec108
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-10 17:31:39 +0200

    (fix2) source Config.in

--- knot/knot.mk
+++ knot/knot.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 KNOT_VERSION = 3.1.7
-KNOT_SITE = https://secure.nic.cz/files/knot-dns/
+KNOT_SITE = https://secure.nic.cz/files/knot-dns
 KNOT_SOURCE = knot-$(KNOT_VERSION).tar.xz
 KNOT_DEPENDENCIES = gnutls libedit liburcu
 
