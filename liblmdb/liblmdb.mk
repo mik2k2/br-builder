ba7819d6e3e96690c9e6ac63b463124e0c744572
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-11 20:34:25 +0200

    library directory for install

--- liblmdb/liblmdb.mk
+++ liblmdb/liblmdb.mk
@@ -13,7 +13,7 @@ define LIBLMDB_BUILD_CMDS
 endef
 
 define LIBLMDB_INSTALL_TARGET_CMDS
-    $(MAKE) -C $(@D) install
+    $(MAKE) -C $(@D)/libraries/liblmdb install
 endef
 
 $(eval $(generic-package))
