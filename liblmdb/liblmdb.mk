ac64d8407eb7db2c125890711a008c834b74ae1e
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-11 20:23:43 +0200

    library directory

--- liblmdb/liblmdb.mk
+++ liblmdb/liblmdb.mk
@@ -9,7 +9,7 @@ LIBLMDB_SITE = https://git.openldap.org/openldap/openldap.git
 LIBLMDB_SITE_METHOD = git
 
 define LIBLMDB_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/libraries/liblmdb all
 endef
 
 define LIBLMDB_INSTALL_TARGET_CMDS
