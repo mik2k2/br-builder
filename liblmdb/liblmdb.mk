da72fa085d8072b41f3ded11f4f5bb5f06eb4f72
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-13 15:21:08 +0200

    set INSTALL_STAGING

--- liblmdb/liblmdb.mk
+++ liblmdb/liblmdb.mk
@@ -7,6 +7,7 @@
 LIBLMDB_VERSION = mdb.master
 LIBLMDB_SITE = https://git.openldap.org/openldap/openldap.git
 LIBLMDB_SITE_METHOD = git
+LIBLMDB_INSTALL_STAGING = YES
 
 define LIBLMDB_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/libraries/liblmdb all
