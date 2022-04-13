0a5b47c4f1fa6f5969253fb1005c98803aa954a9
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2022-04-13 10:30:46 +0200

    /usr/local instead of /usr

--- liblmdb/liblmdb.mk
+++ liblmdb/liblmdb.mk
@@ -13,7 +13,7 @@ define LIBLMDB_BUILD_CMDS
 endef
 
 define LIBLMDB_INSTALL_TARGET_CMDS
-    $(MAKE) prefix=$(TARGET_DIR)/usr -C $(@D)/libraries/liblmdb install
+    $(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D)/libraries/liblmdb install
 endef
 
 $(eval $(generic-package))
