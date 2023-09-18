6032c23e11c9694a64845cfe6051bf0b166743a5
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2023-09-18 19:04:22 +0200

    update Knot to 3.3.1

--- knot/knot.mk
+++ knot/knot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_VERSION = 3.1.9
+KNOT_VERSION = 3.3.1
 KNOT_SITE = https://secure.nic.cz/files/knot-dns
 KNOT_SOURCE = knot-$(KNOT_VERSION).tar.xz
 KNOT_DEPENDENCIES = gnutls libedit liburcu liblmdb
