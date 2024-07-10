1b197040552a8818968bedc76aa6055c056b52e5
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2024-07-10 17:04:31 +0200

    actually enable it

--- lyrebird/lyrebird.mk
+++ lyrebird/lyrebird.mk
@@ -1,6 +1,6 @@
 LYREBIRD_VERSION = 0.2.0
 LYREBIRD_SOURCE = lyrebird-lyrebird-$(LYREBIRD_VERSION).tar.gz
-LYREBIRD_SITE = https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/lyrebird/-/archive/lyrebird-$(LYREBIRD_VERSION)/
+LYREBIRD_SITE = https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/lyrebird/-/archive/lyrebird-$(LYREBIRD_VERSION)
 
 # according to nixpkgs
 LYREBIRD_LICENSE = BSD-2-Cluase, BSD-3-Clause, GPL-3.0+
