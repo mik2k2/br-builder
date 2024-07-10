2fb0fd0292984c104f8b4076d0ef50af3e20a41b
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2024-07-10 16:34:00 +0200

    try adding lyrebird (obfs4)

---
+++ lyrebird/lyrebird.mk
@@ -0,0 +1,11 @@
+LYREBIRD_VERSION = 0.2.0
+LYREBIRD_SOURCE = lyrebird-lyrebird-$(LYREBIRD_VERSION).tar.gz
+LYREBIRD_SITE = https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/lyrebird/-/archive/lyrebird-$(LYREBIRD_VERSION)/
+
+# according to nixpkgs
+LYREBIRD_LICENSE = BSD-2-Cluase, BSD-3-Clause, GPL-3.0+
+LYREBIRD_LICENSE_FILES = LICENSE LICENSE-GPL3.txt
+
+LYREBIRD_BUILD_TARGETS = cmd/lyrebird
+
+$(eval $(golang-package))
